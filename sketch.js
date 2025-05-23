2a0fa5520494e370a2e1640e4021799dd6b721a9
Shane Hendrickson <shane.hendrickson@gmail.com>
2025-05-22 20:41:37 -0400

    acceleration and to mouse movement

--- sketch.js
+++ sketch.js
@@ -1,26 +1,68 @@
-let position
-let velocity 
+class Mover {
+  constructor(){
+    this.position = createVector(random(width), random(height))
+    this.velocity = createVector(random(-2,2), random(-2,2))
+    this.diameter = random(24, 60)
+    this.fill = color(random(0,255), random(0,255), random(0, 255))
+    this.topSpeed = 10
+    // this.acceleration1 = createVector(0,0.1)    
+  }
 
-let circleD = 48
+  
+
+  update(){
+    // this.velocity.add(this.acceleration1)
+
+    // Note that random2D is a static method, meaning it's associated with the Class Vector as a whole, not an instance of the class
+    // this.acceleration2 = p5.Vector.random2D()
+    // this.acceleration2.mult(random(2))
+
+    
+    let mouse = createVector(mouseX, mouseY)
+    let direction = p5.Vector.sub(mouse, this.position)
+    
+    direction.normalize()
+    direction.mult(0.2)
+    this.acceleration = direction
+    
+    this.velocity.add(this.acceleration)
+    this.velocity.limit(this.topSpeed)
+    this.position.add(this.velocity)
+  }
+
+  show(){
+    stroke(0)
+    fill(this.fill)
+    circle(this.position.x, this.position.y, this.diameter)
+  }
+
+  checkEdges(){
+    if (this.position.x > width - (this.diameter / 2) || this.position.x < 0 + (this.diameter / 2)){
+      this.velocity.x = this.velocity.x * -1
+    }
+
+    if (this.position.y > height - (this.diameter / 2) || this.position.y < 0 + (this.diameter / 2)){
+    this.velocity.y = this.velocity.y * -1
+    }
+  }
+
+
+}
+
+let mover
 
 function setup() {
-  createCanvas(640, 240);
-  position = createVector(100, 100)
-  velocity = createVector(1, 3.3)
+  createCanvas(windowWidth, windowHeight);
+  mover = new Mover()
 }
 
 function draw() {
   background(0);
-  position.add(velocity)
+  mover.show()
+  mover.update()
+  mover.checkEdges()
+}
 
-  if (position.x > width - (circleD / 2) || position.x < 0 + (circleD / 2)){
-    velocity.x = velocity.x * -1
-  }
-  if (position.y > height - (circleD / 2) || position.y < 0 + (circleD / 2)){
-    velocity.y = velocity.y * -1
-  }
-
-  stroke(0)
-  fill(127)
-  circle(position.x, position.y, circleD)
+function windowResized(){
+  resizeCanvas(windowWidth, windowHeight)
 }
